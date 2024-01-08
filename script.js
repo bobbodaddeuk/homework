51a8459b3bc0bb838430c6c9c755d4ba9fe78cbf
TriumphOne <woo001232naver.com>
2024-01-08 19:41:20 +0900

    reset.css, .prettierrc 적용

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjlhMmI2MjBiNjMwYjViM2RmYWEyMzBhNDIzODFiMCIsInN1YiI6IjY1OTNjZGE0Y2U0ZGRjNmU2NDdkZGIyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4CSbF2T53qjlLENtBlffmhp-YxvcN7oE5otcgSdDpHc'
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjlhMmI2MjBiNjMwYjViM2RmYWEyMzBhNDIzODFiMCIsInN1YiI6IjY1OTNjZGE0Y2U0ZGRjNmU2NDdkZGIyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4CSbF2T53qjlLENtBlffmhp-YxvcN7oE5otcgSdDpHc"
   }
 };
 
-let url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+let url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
 
 function card() {
   fetch(url, options)
-    .then(response => response.json())
-    .then(response => {
-      let rows = response['results']
-      rows.forEach(a => {
-        let id = a['id'];
-        let title = a['title'];
-        let overView = a['overview'];
-        let posterPath = a['poster_path'];
-        let voteAverage = a['vote_average'];
+    .then((response) => response.json())
+    .then((response) => {
+      let rows = response["results"];
+      rows.forEach((a) => {
+        let id = a["id"];
+        let title = a["title"];
+        let overView = a["overview"];
+        let posterPath = a["poster_path"];
+        let voteAverage = a["vote_average"];
         let temp_html = `
         <div class='card' onclick="getId(${id})">
             <img class='movieImg' src="https://image.tmdb.org/t/p/w500${posterPath}" alt="${title} Poster">
@@ -27,33 +28,37 @@ function card() {
             <p class='voteAverage'>평점 : <b>${voteAverage}</b></p>
         </div>
         `;
-        let element = document.getElementById('names-q1');
+        let element = document.getElementById("names-q1");
         element.innerHTML += temp_html;
       });
     })
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
 }
-card()
+card();
 
 function filter() {
   let search = document.getElementById("searchValue").value.toLowerCase();
   let cards = document.getElementsByClassName("card");
+  let searchResults = 0;
 
   for (let i = 0; i < cards.length; i++) {
-    if (cards[i].getElementsByClassName('movieName')[0].innerText.toLowerCase().indexOf(search) !== -1) {
-      cards[i].style.display = "grid"
+    if (cards[i].getElementsByClassName("movieName")[0].innerText.toLowerCase().indexOf(search) !== -1) {
+      cards[i].style.display = "grid";
+      searchResults++;
     } else {
-      cards[i].style.display = "none"
+      cards[i].style.display = "none";
     }
+  }
+  if (searchResults === 0) {
+    alert("검색 결과가 없습니다.");
   }
 }
 
 function getId(id) {
-  alert("이 영화의 id는 " + id + "입니다.")
+  alert("이 영화의 id는 " + id + "입니다.");
 }
 
-searchForm.addEventListener('submit', (e) => {
-  e.preventDefault()
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
   filter();
-}
-)
+});
